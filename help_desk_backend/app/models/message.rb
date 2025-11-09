fb9e2f219b7631842cbebac939e5e80ddc7c70be
Kaitlyn t <kaitntom@gmail.com>
2025-11-08 21:22:52 -0800

    adding validations

--- help_desk_backend/app/models/message.rb
+++ help_desk_backend/app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 
   validates :content, presence: true
+  validates :conversation, :sender, presence: true
+  validates :sender_role, inclusion: {in: %w[initiator expert]}
 end
